a63ca16817f6f297b739bb952494bece675e9c42
NavyaKolisetty <134955848+NavyaKolisetty@users.noreply.github.com>
2026-04-24 16:15:17 +0530

    Update AWS region and GitHub variables in Terraform

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,34 +1,36 @@
 variable "aws_region" {
-  description = "AWS region where resources will be created"
+  description = "AWS region"
   type        = string
-  default     = "us-east-1"
-  # We use us-east-1 because it supports all AWS services we need
+  default     = "ap-south-1"
 }
- 
+
 variable "project_name" {
-  description = "Name of the project - used to name all resources"
+  description = "Project name"
   type        = string
   default     = "static-website"
-  # This name will appear in S3 bucket name, CodePipeline name, etc.
 }
- 
+
 variable "github_repo_owner" {
-  description = "Your GitHub username"
+  description = "GitHub username"
   type        = string
-  default     = "YOUR_GITHUB_USERNAME"
-  # IMPORTANT: Replace with your actual GitHub username
+  default     = "NavyaKolisetty"
 }
- 
+
 variable "github_repo_name" {
   description = "GitHub repository name"
   type        = string
-  default     = "static-website-cicd"
-  # This should match the repo name you created on GitHub
+  default     = "use-case"
 }
- 
+
 variable "github_branch" {
-  description = "GitHub branch to watch for changes"
+  description = "GitHub branch"
   type        = string
   default     = "main"
-  # Pipeline will trigger when you push to this branch
 }
+
+variable "github_connection_arn" {
+  description = "Existing GitHub connection ARN"
+  type        = string
+  default     = "arn:aws:codeconnections:ap-south-1:239359658737:connection/2ac88a31-9565-412b-9ab9-a2bcde9f4a7d"
+}
+
